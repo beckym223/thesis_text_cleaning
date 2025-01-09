fa63728e9d5e5f1217e679fa0bfe12d55f595e93
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 15:51:30 -0800

    added apply func to text dir

--- python_scripts/utils.py
+++ python_scripts/utils.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 import logging
-from typing import Optional, Any, Callable
+from typing import Optional, Any, Callable,Concatenate
 import shutil
 import functools
 
@@ -103,5 +103,62 @@ def commit(_func:Optional[Callable]=None,
         return decorator_commit
     else:
         return decorator_commit(_func)
+    
+def apply_func_to_txt_dir(source_dir:str,
+                          dest_dir:str,
+                          func:Callable[Concatenate[str,...],str],
+                          skip_if_exists=False,
+                          pass_filename=False,
+                          *args, **kwargs):
+    """
+    Applies a given function to the contents of all .txt files in a directory tree and writes the 
+    results to a new directory structure, maintaining the same relative folder structure.
+
+    Parameters:
+    -----------
+    start_dir_path : str
+        The root directory containing subdirectories and .txt files to process.
+    write_to : str
+        The root directory where the processed .txt files will be written.
+    func : function
+        The function to apply to the contents of each .txt file. It should take the file's text 
+        content as its first argument and return the modified content.
+    skip_if_exists : bool, optional
+        If True, the function will skip files that already exist in the write_to directory.
+        Defaults to False.
+    pass_filename : bool, optional
+        If True, the filename will also be passed to the function along with the file content.
+        Defaults to False.
+    *args : tuple
+        Additional positional arguments to pass to the `func`.
+    **kwargs : dict
+        Additional keyword arguments to pass to the `func`.
+
+    Returns:
+    --------
+    None
+    """
+    for file_name in sorted(os.listdir(source_dir)):
+        if file_name[0] ==".":
+            continue
+        src_path = os.path.join(source_dir,file_name)
+        new_file_path = os.path.join(dest_dir,file_name)
+   
+        if os.path.exists(new_file_path) and skip_if_exists:
+            continue
+        
+        # Open the original file and read its content
+        with open(src_path, 'r') as f:
+            old_text = f.read()
+            
+        # Apply the provided function to the text content (with or without filename)
+        if pass_filename:
+            new_text = func(old_text, file_name, *args, **kwargs)  # Pass file name
+        else:
+            new_text = func(old_text, *args, **kwargs)  # Only pass text content
+        
+    # Write the modified text to the new file path
+        with open(new_file_path, 'w') as f:
+            f.write(new_text)
 
 
