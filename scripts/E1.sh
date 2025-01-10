60724ca8f411a68eb5b6cb2fbc7b79d098e60540
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 16:59:44 -0800

    updated log path

--- scripts/E1.sh
+++ scripts/E1.sh
@@ -3,7 +3,7 @@ NAME="E1"
 # Directories and log file for E2
 SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/data/groups/$NAME/texts"
 DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/clean_text/$NAME"
-LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/cleaning_by_group/logs/$NAME.log"
+LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/logs/$NAME.log"
 PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/python_scripts/${NAME}.py"
 
 # Call the common script
