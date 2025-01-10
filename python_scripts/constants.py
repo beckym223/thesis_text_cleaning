9afc30ed63dfc73303d9af5d723f77ad9cd8163d
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 18:04:51 -0800

    added e2 footnotes

---
+++ python_scripts/constants.py
@@ -0,0 +1,13 @@
+E2_FOOT_LINES = {
+    "Economics-1893-0-01.txt": [28],
+    "Economics-1894-0-05.txt": [29, 32],
+    "Economics-1894-0-06.txt": [33],
+    "Economics-1894-0-07.txt": [34],
+    "Economics-1894-0-11.txt": [36],
+    "Economics-1894-0-12.txt": [35],
+    "Economics-1894-0-14.txt": [28],
+    "Economics-1894-0-15.txt": [28],
+    "Economics-1894-0-16.txt": [35],
+    "Economics-1894-0-19.txt": [29],
+    "Economics-1903-0-08.txt": [],
+}
