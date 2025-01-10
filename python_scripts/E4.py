f3365f37e8234d69b7c889c78f470507ed5e95bb
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 19:33:29 -0800

    wrote E4 script

--- python_scripts/E4.py
+++ python_scripts/E4.py
@@ -3,27 +3,45 @@ import re
 import logging
 from utils import *
 from text_cleaning import *
-from constants import E3_FOOT_LINES
+from constants import E4_ABSTRACT_DICT, E4_FOOT_LINES
 
 def handle_first_page(file:str, text:str)->str:
-    disc,year,num,pagetxt = file.split("-")
     lines = text.split("\n")
-    line_num = 0
-    if int(year)<1912:
-        num_lowercase_lines =0
-        while num_lowercase_lines<2:
-            line = lines[line_num]
-            if re.search(r"[a-z]{3,}",line) is not None:
-                num_lowercase_lines+=1
-            else:
-                num_lowercase_lines = 0
-            line_num+=1
-        start_line = line_num-1  
+    save_path = os.path.join(dest_dir,file)
+    after_abstract = E4_ABSTRACT_DICT.get(file)
+    if after_abstract is not None:
+        for i,line in enumerate(lines):
+            if line.startswith(after_abstract):
+                lines = lines[i:]
+                break
+        #→find the line that starts with that text
+        #delete everything before it
     else:
-        while re.search(r"\b[a-z]+\b", lines[line_num]) is None:
-            line_num+=1
-        start_line = line_num+1
-    return "\n".join(lines[start_line:])
+        all_caps_line=0
+        #finding first all caps line
+        while re.search('[a-z]',lines[all_caps_line]) is not None:
+            all_caps_line+=1
+        #finding line with not all caps
+        first_real_line = all_caps_line+1
+        while re.search(r"[a-z]", lines[first_real_line]) is None:
+            first_real_line+=1
+        if re.search("Presidential [aA]ddress",lines[first_real_line]) is not None:
+            first_real_line+=2
+            if "1933-0" in file or "1934-0" in file:
+                first_real_line+=1
+            stop_line = None
+        else: #means presidential address in footnote
+            stop_line = first_real_line
+            try:
+                while "Presidential address" not in lines[stop_line]:
+                    stop_line+=1
+            except:
+                print(f"Presidential address not found in footnote for {file}")
+                with open(save_path,'w') as f:
+                    f.write("\n".join(lines))
+        lines = lines[first_real_line:stop_line]
+    return "\n".join(lines)
+
 def clean_headers_footers(dest_dir:str,commit_changes:bool):
     file:str
     try:
@@ -53,6 +71,11 @@ def clean_headers_footers(dest_dir:str,commit_changes:bool):
         raise
     if commit_changes:
         git_commit(dest_dir,"Cleaned headers and footers")
+
+def is_page_to_remove(file:str)->bool:
+    disc,year,num,pagetxt = file.split("-")
+    page=int(pagetxt[:-4])
+    return page==0 or ('1933-1' in file and page>=9)
     
 
 def main(source_dir:str, dest_dir:str, log_file:str, commit_changes:bool):
@@ -61,11 +84,11 @@ def main(source_dir:str, dest_dir:str, log_file:str, commit_changes:bool):
 
     initialize_directories(source_dir,dest_dir,commit_changes)
 
-    remove_files(dest_dir,is_first_page,commit_changes)
+    remove_files(dest_dir,is_page_to_remove,commit_changes)
 
     clean_headers_footers(dest_dir,commit_changes)
 
-    remove_footnote_lines(dest_dir,E3_FOOT_LINES,commit_changes)
+    remove_footnote_lines(dest_dir,E4_FOOT_LINES,commit_changes)
 
     fix_dash_errors_in_dir(dest_dir,commit_changes)
     
