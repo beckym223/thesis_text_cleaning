c8a2cd67b357c761493a6c8fc31cbc4fc0d87a28
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 16:35:40 -0800

    fixed file paths

--- scripts/E1.sh
+++ scripts/E1.sh
@@ -2,9 +2,9 @@
 NAME="E1"
 # Directories and log file for E2
 SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/data/groups/$NAME/texts"
-DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/data/groups_cleaned/$NAME"
+DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/clean_text/$NAME"
 LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/cleaning_by_group/logs/$NAME.log"
-PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/cleaning_by_group/python_scripts/${NAME}_cleaning.py"
+PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/python_scripts/${NAME}_cleaning.py"
 
 # Call the common script
 /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT"
