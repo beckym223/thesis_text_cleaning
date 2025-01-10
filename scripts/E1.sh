d50a66ff9183788b697ea7a9982b4d530bd77218
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 16:39:17 -0800

    fixed name again

--- scripts/E1.sh
+++ scripts/E1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 NAME="E1"
 # Directories and log file for E2
-SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/data/groups/$NAME/texts"
-DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/clean_text/$NAME"
-LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/cleaning_by_group/logs/$NAME.log"
-PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/python_scripts/${NAME}.py"
+SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/data/groups/$NAME/texts"
+DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/clean_text/$NAME"
+LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/cleaning_by_group/logs/$NAME.log"
+PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/python_scripts/${NAME}.py"
 
 # Call the common script
 /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT"
+/Users/BeckyMarcusMacbook/Thesis/TextCleaning/python_scripts/E1.py
