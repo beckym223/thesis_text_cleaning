bac9ed17f7879bb7d6df6f4792f49c1d5394cdf4
beckym223 <marcu22r@mtholyoke.edu>
2025-01-09 16:36:20 -0800

    fixed file name

--- scripts/E1.sh
+++ scripts/E1.sh
@@ -4,7 +4,7 @@ NAME="E1"
 SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/data/groups/$NAME/texts"
 DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/clean_text/$NAME"
 LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/cleaning_by_group/logs/$NAME.log"
-PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/python_scripts/${NAME}_cleaning.py"
+PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/EconTextCleaning/python_scripts/${NAME}.py"
 
 # Call the common script
 /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT"
