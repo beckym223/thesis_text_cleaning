cbc32eafe90ecec6d58ef02ccc9062dbc0f02a0d
beckym223 <marcu22r@mtholyoke.edu>
2025-01-21 11:33:03 -0800

    simplified it

--- scripts/test_pipeline.sh
+++ scripts/test_pipeline.sh
@@ -17,23 +17,24 @@ rm -rf "{$DEST_DIR:?}/*"
 
 echo "Running test scenario: $SCENARIO"
 
-case "$SCENARIO" in
-    success)
-        echo "Simulating a successful pipeline run..."
-        /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh -b "test" "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT" "$@"
-        ;;
-    failure)
-        echo "Simulating a failed pipeline run..."
-        /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT" --fail -b "test" "$@"
-        ;;
-    no_commit)
-        echo "Simulating changes made but not committed..."
-        /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh  "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT" --no-commit -b "test" "$@"
-        ;;
-    *)
-        echo "Invalid scenario. Choose from: success, failure, no_commit"
-        exit 1
-        ;;
-esac
+/Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh -b "test" "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT" "$@"
+
+# case "$SCENARIO" in
+#     success)
+#         echo "Simulating a successful pipeline run..."
+#         ;;
+#     failure)
+#         echo "Simulating a failed pipeline run..."
+#         /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT" --fail -b "test" "$@"
+#         ;;
+#     no_commit)
+#         echo "Simulating changes made but not committed..."
+#         /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh  "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT" --no-commit -b "test" "$@"
+#         ;;
+#     *)
+#         echo "Invalid scenario. Choose from: success, failure, no_commit"
+#         exit 1
+#         ;;
+# esac
 
 echo "Test completed. Check the log file for details: $LOG_FILE"
