0c23c1f54efc9bfe766c1d4aaf6710b2feb153ca
beckym223 <marcu22r@mtholyoke.edu>
2025-02-02 18:18:40 -0500

    Some formatting, time logging

--- networking/fixing_leftovers.py
+++ networking/fixing_leftovers.py
@@ -164,122 +164,124 @@ def known_enough(word:str,threshold:float)->tuple[bool,float]:
 
 
 
-def run_cycle(unknown_words:set[str], chars: list[tuple[str, str]], known_corrections, max_iteration: int, logger, G:DiGraph,threshold=math.e):
+def run_cycle(unknown_words: set[str], chars: list[tuple[str, str]], known_corrections, max_iteration: int, logger, G: DiGraph, threshold=math.e):
     transformed_chars = [(rf"(?=({old}))", new) for old, new in chars]
     logger.info("Initiating unknown words in graph")
     results = {}
     new_words_for_next_iter = deque(unknown_words)
 
     for word in unknown_words:
-        G.add_node(word, root=word, final=None, last_change_iter=0,match_word=word)
-    if max_iteration==-1:
-        max_iteration=100
+        G.add_node(word, root=word, final=None, last_change_iter=0, match_word=word)
+    if max_iteration == -1:
+        max_iteration = 100
+    
     current_iter = 0
-    any_change=True
+    any_change = True
+    start_time = time.time()
+    
     try:
         while current_iter < max_iteration:
-            any_change=False
+            any_change = False
             start_results = len(results)
             current_iter += 1
+            iter_start_time = time.time()
             logger.notice("Starting iteration %s", current_iter)
 
-            to_remove=set()
+            to_remove = set()
             next_char_queue = new_words_for_next_iter
             new_words_for_next_iter = deque()
 
             for old, new in transformed_chars:
+                char_start_time = time.time()
                 logger.info("Processing character replacement: %s -> %s", old, new)
                 char_queue = next_char_queue
                 next_char_queue = deque()
 
                 while char_queue:
-                    node_word = char_queue.popleft()  # Faster than .pop() for queue operations
+                    node_word = char_queue.popleft()
                     node_data = G.nodes[node_word]
-                    
-
                     match_word = node_data['match_word']
                     root = node_data["root"]
 
-
-                    if node_data['last_change_iter']<current_iter-1 and not node_data['final']:
-                        logger.info("%s not changeable, continuing and removing")
+                    if node_data['last_change_iter'] < current_iter - 1 and not node_data['final']:
+                        logger.info("%s not changeable, continuing and removing", node_word)
                         to_remove.add(node_word)
                         continue
-
 
                     if root in results:
                         continue
 
-                    
-
-                    node_solved=False
-                    matches = [(m.start(1), len(m.group(1))) for m in re.finditer(old, match_word)]  # Store both index & match length
-                    i=None
+                    node_solved = False
+                    matches = [(m.start(1), len(m.group(1))) for m in re.finditer(old, match_word)]
+                    i = None
                     for i, match_length in matches:
-                        any_change=True
+                        any_change = True
                         new_match = apply_t(node_word, new, i, match_length)
                         new_word = new_match.lower()
                         if new_word in FALSE_POSITIVES:
                             continue
                         if new_word not in G:
-                            known, zfreq = known_enough(new_word,threshold)
+                            known, zfreq = known_enough(new_word, threshold)
                             previously_corrected = known_corrections.get(new_word)
 
                             if previously_corrected:
-
                                 logger.info("Previously corrected: '%s' -> '%s'", root, previously_corrected)
                                 node_data["final"] = previously_corrected
-                                G.add_node(new_word, root=root, final=previously_corrected,match_word=new_match,last_change_iter=current_iter)
+                                G.add_node(new_word, root=root, final=previously_corrected, match_word=new_match, last_change_iter=current_iter)
                                 results[root] = previously_corrected
                                 node_solved = True
                             elif known:
                                 results[root] = new_word
-                                
                                 node_data["final"] = new_word
-                                G.add_node(new_word, root=root, final=new_word, freq=zfreq,match_word=new_match,last_change_iter=current_iter)
+                                G.add_node(new_word, root=root, final=new_word, freq=zfreq, match_word=new_match, last_change_iter=current_iter)
                                 node_solved = True
                                 logger.info("New valid word: '%s' (freq: %.2f)", new_word, zfreq)
                             else:
-                                G.add_node(new_word, root=root, final=None, freq=zfreq,match_word=new_match,last_change_iter=current_iter)
+                                G.add_node(new_word, root=root, final=None, freq=zfreq, match_word=new_match, last_change_iter=current_iter)
                                 new_words_for_next_iter.append(new_word)
                                 logger.debug("Added to graph: %s", new_word)
-
                             G.add_edge(node_word, new_word, old=old, new=new, i=i)
 
                         else:
-                            found_node =G.nodes[new_word]
+                            found_node = G.nodes[new_word]
                             final = found_node.get("final")
                             if final:
                                 results[root] = final
                                 node_data["final"] = final
                                 node_solved = True
-
                             elif not G.has_edge(node_word, new_word):
-                                found_node['match_word']=combine_match(new_match,found_node['match_word'])
+                                found_node['match_word'] = combine_match(new_match, found_node['match_word'])
                                 G.add_edge(node_word, new_word, old=old, new=new, i=i)
 
                         if node_solved:
                             break
+                    
                     if i is not None:
                         node_data['last_change_iter'] = current_iter
                     next_char_queue.append(node_word)
-                ## End of char thing
-                logger.info("End of char thing, removing %d nodes",len(to_remove))
-                G.remove_nodes_from(to_remove)
-                to_remove.clear()
+                
+                char_end_time = time.time()
+                logger.info("Processed '%s -> %s' in %.2f seconds", old, new, char_end_time - char_start_time)
 
             logger.notice("%d results found in iteration %d, total %d", len(results) - start_results, current_iter, len(results))
-            prune_graph(G,results,next_char_queue,new_words_for_next_iter,logger)
+            prune_graph(G, results, next_char_queue, new_words_for_next_iter, logger)
             if not any_change:
-                logger.notice("Ending at iteration %d due to lack of change",current_iter)
+                logger.notice("Ending at iteration %d due to lack of change", current_iter)
                 break
+            
+            iter_end_time = time.time()
+            logger.info("Iteration %d completed in %.2f seconds", current_iter, iter_end_time - iter_start_time)
+        
     except KeyboardInterrupt:
-        logger.notice("Keyboard interupt at iteration %s", current_iter)
+        logger.notice("Keyboard interrupt at iteration %s", current_iter)
     finally:
-        still_out_there = set([x for x in unknown_words if x not in results])
+        total_time = time.time() - start_time
+        logger.info("Total runtime: %.2f seconds", total_time)
+        still_out_there = {x for x in unknown_words if x not in results}
         logger.info("Total results: %d", len(results))
-        clean_unconnected_nodes(G,results,logger)
+        clean_unconnected_nodes(G, results, logger)
         return G, results, still_out_there
+
 
 def check_nodes(G:DiGraph,results:dict,logger):
     possible_children:dict[str,list[tuple[str,float]]] = defaultdict(list)
