15af3abb8bd71c6784cff30c5af5966d3808c910
beckym223 <marcu22r@mtholyoke.edu>
2025-02-10 22:31:42 -0500

    fixed footnotes, special case with 1907

--- python_scripts/E3_updated.py
+++ python_scripts/E3_updated.py
@@ -4,6 +4,7 @@ import logging
 from utils import *
 from text_cleaning import *
 from constants import E3_FOOT_LINES
+import numpy as np
 
 def handle_first_page(file:str, text:str)->str:
     disc,year,num,pagetxt = file.split("-")
@@ -47,6 +48,12 @@ def clean_headers_footers(dest_dir:str,commit_changes:bool):
                     text = "\n".join(no_header)
                 else:
                     text = handle_first_page(file,text)
+                if file=="Economics-1907-0-05.txt":
+                    lines= text.splitlines()
+                    to_change = np.array(lines[:10])
+                    order =  [3,4,5,6,7,0,8,1,9,2]
+                    text = "\n".join([" ".join(to_change[order]),*lines[10:]])
+
                 with open(path,'w') as f:
                     f.write(text.strip())
             except Exception as e:
@@ -57,6 +64,37 @@ def clean_headers_footers(dest_dir:str,commit_changes:bool):
     if commit_changes:
         git_commit(dest_dir,"Cleaned headers and footers")
     
+def find_footnote_lines(dest_dir,commit_changes):
+    for file in E3_FOOT_LINES:
+        path = os.path.join(dest_dir,file)
+        with open(path,'r') as f:
+            text = f.read()
+        lines = text.splitlines()
+        p = re.compile(r"^\s?(¹|\*|[A-Z]([a-z]+|\.)? [A-Z]\.)|'|\d{1,2}|System of")
+        fn_start = None
+        i = -1
+        since_last=0
+        while i>=-1*len(lines) and since_last<5:
+            line = lines[i]
+            if p.match(line):
+                since_last=0
+                fn_start = i
+                lines[i] = f"{i}-->{line}"
+                
+            else:
+                since_last+=1
+            i-=1
+
+        if fn_start is not None:
+            with open(path,'w') as f:
+                f.write('\n'.join(lines[:fn_start]))
+        else:
+            logging.warning(f"Could not find footnote for {path}")
+    if commit_changes:
+        git_commit(dest_dir,"Removed footnote lines")
+
+    
+            
 
 def main(source_dir:str, dest_dir:str, log_file:str, commit_changes:bool):
 
@@ -68,13 +106,13 @@ def main(source_dir:str, dest_dir:str, log_file:str, commit_changes:bool):
 
     clean_headers_footers(dest_dir,commit_changes)
 
-    # # remove_footnote_lines(dest_dir,E3_FOOT_LINES,commit_changes)
+    find_footnote_lines(dest_dir,commit_changes)
 
-    # # fix_dash_errors_in_dir(dest_dir,commit_changes)
+    fix_dash_errors_in_dir(dest_dir,commit_changes)
     
-    # # handle_line_breaks_across_pages(dest_dir,commit_changes=True)
+    handle_line_breaks_across_pages(dest_dir,commit_changes=True)
 
-    # # split_into_paras_at_length(dest_dir,40,commit_changes)
+    split_into_paras_at_length(dest_dir,40,commit_changes)
 
 if __name__ == "__main__":
     import sys
