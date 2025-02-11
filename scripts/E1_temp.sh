bf1a85cadacd5759b0c073bb577f39916ac405d5
beckym223 <marcu22r@mtholyoke.edu>
2025-02-10 20:06:38 -0500

    fixed path

--- scripts/E1_temp.sh
+++ scripts/E1_temp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME="E1"
 # Directories and log file for E2
-SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/data/unmodified_text/$NAME/"
+SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/data/gcloud_splits/unmodified_text/$NAME/"
 DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/new_clean_text/$NAME"
 LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/logs/$NAME.log"
 PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/python_scripts/${NAME}.py"
