9651ae6adbc1df9db4eceaf8f6685e9df1dcb9e2
beckym223 <marcu22r@mtholyoke.edu>
2025-02-10 20:10:07 -0500

    changed path to new python script

--- scripts/E1_temp.sh
+++ scripts/E1_temp.sh
@@ -4,7 +4,7 @@ NAME="E1"
 SOURCE_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/data/gcloud_splits/unmodified_text/$NAME/"
 DEST_DIR="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/new_clean_text/$NAME"
 LOG_FILE="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/logs/$NAME.log"
-PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/python_scripts/${NAME}.py"
+PYTHON_SCRIPT="/Users/BeckyMarcusMacbook/Thesis/TextCleaning/python_scripts/${NAME}_updated.py"
 
 # Call the common script
 /Users/BeckyMarcusMacbook/Thesis/TextCleaning/scripts/run_pipeline.sh "$SOURCE_DIR" "$DEST_DIR" "$LOG_FILE" "$PYTHON_SCRIPT"
